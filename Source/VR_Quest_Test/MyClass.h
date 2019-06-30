4699530464907da0294ba96dbd480afca9864d5d
japanman <sutesuteakau@yahoo.co.jp>
2019-07-01 00:58:25 +0900

    vrm plugin
    vrm plugin

---
+++ Source/VR_Quest_Test/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VR_QUEST_TEST_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
---
+++ Source/VR_Quest_Test/VR_Quest_Test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_Quest_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR_Quest_Test, "VR_Quest_Test" );
